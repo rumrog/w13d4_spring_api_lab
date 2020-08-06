b1a14d3bbfef4e7f9736688d9d5fd1f246b05fe5
Rumen Roger <rumenroger@gmail.com>
2020-08-06 09:41:59 +0100

    Initial commit

---
+++ src/main/java/com/codeclan/example/w13d4_spring_api_lab/W13d4SpringApiLabApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.w13d4_spring_api_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W13d4SpringApiLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(W13d4SpringApiLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeclan/example/w13d4_spring_api_lab/W13d4SpringApiLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.w13d4_spring_api_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W13d4SpringApiLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
